51f33f09749357b64bde5fd4464a050af16a7217
Barb <clockery69@gmail.com>
2021-02-24 10:07:25 +0100

    added CSS rules

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,9 @@
 import document from "document";
 
-let text = document.getElementById("text") as TextElement; // not displayed, evtl inherit text/props from here
-let shadow = document.getElementById("shadow") as TextElement;
-let highlight = document.getElementById("highlight") as TextElement;
-let main = document.getElementById("main") as TextElement;
+let text = document.getElementById("text") as TextElement;              // not displayed, evtl inherit text/props from here
+let shadow = document.getElementById("shadow") as TextElement;          // in CSS reachable as #textShadow #shadow
+let highlight = document.getElementById("highlight") as TextElement;    // in CSS reachable as #textShadow #highlight
+let main = document.getElementById("main") as TextElement;              // in CSS reachable as #textShadow #main
 
 
 
@@ -28,11 +28,11 @@ document.getElementsByClassName("myText").forEach(e => {
 // individual settings for all 3 textElements, later per set? class? inline?
 // offset on x,y
 main.x = 0; // takes x from use
-highlight.x = - 1; // offset to main
+//highlight.x = - 1; // offset to main
 shadow.x =  2; // offset to main
 
 main.y = 0;
-highlight.y = - 1;
+//highlight.y = - 1;
 shadow.y =  2;
 
 // fill
