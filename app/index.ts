eb403b953d8629f970cee7378e6e403e0d5acfd5
Barb <clockery69@gmail.com>
2021-02-24 08:49:30 +0100

    comments

--- app/index.ts
+++ app/index.ts
@@ -4,70 +4,42 @@ let text = document.getElementById("text") as TextElement;
 let shadow = document.getElementById("shadow") as TextElement;
 let highlight = document.getElementById("highlight") as TextElement;
 let main = document.getElementById("main") as TextElement;
-let textShadow = document.getElementById("textShadow");
+//let textShadow = document.getElementById("textShadow");
 
 
-
+// settings text-props : evtl inherited?
 text.text = "shadow-text";
 text.style.fontFamily = "Barlow-Bold";
 text.style.fontSize = 45;
 text.textAnchor = "middle"  ?? "start";
 text.letterSpacing = 0 ?? 0;
 
-let fs: number;
-fs = 70;
-
-let a: number;
-a= 168;
-
-main.x = a;
-highlight.x = a - 1;
-shadow.x = main.x + 1;
-
-let b: number;
-
-b = 168;
-
-main.y = b;
-highlight.y = b - 1;
-shadow.y = b + 1;
-
+// now applied per class
 document.getElementsByClassName("myText").forEach(e => {
   (e as TextElement).text = text.text;
   (e as TextElement).style.fontFamily = text.style.fontFamily;
   (e as TextElement).style.fontSize = text.style.fontSize;
   (e as TextElement).textAnchor = text.textAnchor;
   (e as TextElement).letterSpacing = text.letterSpacing;
-  
-
-  
 });
 
-/*
-import { me as appbit } from "appbit";
-import { minuteHistory, dayHistory, today } from 'user-activity'
+// individual offset on x,y
+main.x = 0; // takes x from use
+highlight.x = - 1; // offset to main
+shadow.x =  2; // offset to main
 
-const steps = today.adjusted.steps
-const now = new Date()
-console.log(now.getMinutes())
-const minuteRecords = minuteHistory.query({ limit: now.getMinutes() })
-console.log(JSON.stringify(minuteRecords))
-const hourlySteps = minuteRecords.reduce((a, b) => a + (b.steps || 0), 0);
-console.log(hourlySteps);
+main.y = 0;
+highlight.y = - 1;
+shadow.y =  2;
 
-if (appbit.permissions.granted("access_activity")) {
-  // query the previous 5 minutes step data
-  const minuteRecords = minuteHistory.query({ limit: now.getMinutes() });
+// individual fill
+main.style.fill = "orange";
+highlight.style.fill = "white";
+shadow.style.fill = "red";
 
-  minuteRecords.forEach((minute, index) => {
-    console.log(`${minute.steps || 0} steps. ${index + 1} minute(s) ago.`);
-  });
+// individual opacity
+main.style.opacity = 1; // individual opacity
+highlight.style.opacity = 0.8;
+shadow.style.opacity = 0.8;
 
-  // query all days history step data
-  const dayRecords = dayHistory.query();
 
-  dayRecords.forEach((day, index) => {
-    console.log(`${day.steps || 0} steps. ${index + 1} day(s) ago.`);
-  });
-}
-*/
