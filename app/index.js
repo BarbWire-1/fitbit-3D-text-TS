9e484fdb9aaf79d0af1ea5f4dd9b99d80cd1f24e
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-02-15 09:42:00 +0100

    moved inspect funs to "devTools"

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ let allLights = document.getElementsByClassName('light');
 
 let cd = 100;
 
-const update = setInterval(() => {
+setInterval(() => {
   test.text = `steps ${today.adjusted.steps}`;
   calsLabel.text = `cals ${today.adjusted.calories}`;
   countDown.text = (`00${--cd}`).slice(-2);
