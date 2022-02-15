352ade37f7a39f8e20c359963ca8fc89fb2451e4
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-02-15 11:38:49 +0100

    added objInspection

---
+++ app/devTools.js
@@ -0,0 +1,43 @@
+//INSPECT OBJECT PROTOTYPE CHAIN
+export function dumpProperties(name, obj, types) {  // This isn't needed; it's just to show how everything links together
+    // types: try to determine type of each property: can cause hard crashes with some objects.
+    let proto = obj
+    let level = 0
+    let type = '?'
+    console.log(`Members of ${name}:`)
+    do {
+      console.log(`  Level ${level++}:`)
+      for(const memberName in proto) {
+        //console.log('in for()')
+        if (proto.hasOwnProperty(memberName)) {
+          //console.log(`in if() ${memberName}`)
+          // memberName 'text' crashes sim
+          if (types)
+            try {
+              //console.log('before obj[]')
+              const member = obj[memberName]  // get member from top-level obj rather than proto, as the latter crashes if not a function
+              //console.log(`in try member=${member}`)
+              type = typeof member
+            } catch(e) {
+              //console.log('in catch')
+              type = 'INACCESSIBLE'
+            }
+          console.log(`    ${memberName} (${type})`)
+        }
+      }
+      proto = Object.getPrototypeOf(proto)
+      console.log('  ---------------')
+    } while (proto)
+  }
+  //call like: dumpProperties('name', obj, boolean)
+  
+  //inspect key:value of object
+  export const inspectObject = (objName, obj) => {
+     
+    for (const prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+         
+        console.log(` ${prop}: ${JSON.stringify(obj[prop])}`)
+      };
+    };
+  };
--- app/index.ts
+++ app/index.ts
@@ -3,6 +3,7 @@ import document from "document";
 import { today } from "user-activity";
 import { widgetFactory } from './widgets/widget-factory';
 import { shadowText, ShadowTextWidget } from './widgets/shadow-text';
+import {dumpProperties,inspectObject} from './devTools'
 //import { ShadowTextWidget } from './widgets/shadow-text/index';
 
 widgetFactory(shadowText);
@@ -44,45 +45,23 @@ test.style.fill = "yellow"//NOT ASSIGNED
 // test.x DOES get applied...tss
 
 
-console.log(`test.light.style.fill: ${test.light.style.fill}`)
-console.log(`test.light.x: ${JSON.stringify(test.light.x)}`)
-console.log("test.main.textAnchor: "+test.main.textAnchor);
+// console.log(`test.light.style.fill: ${test.light.style.fill}`)
+// console.log(`test.light.x: ${JSON.stringify(test.light.x)}`)
+// console.log("test.main.textAnchor: "+test.main.textAnchor);
 
 //allLights.forEach(el => el.style.fill = "black");
 
-console.log(JSON.stringify(allLights))
+//console.log(JSON.stringify(allLights))
 test.light.style.fill = "limegreen"
 
 
 //INSPECT OBJECT PROTOTYPE CHAIN
-function dumpProperties(name, obj, types) {  // This isn't needed; it's just to show how everything links together
-  // types: try to determine type of each property: can cause hard crashes with some objects.
-  let proto = obj
-  let level = 0
-  let type = '?'
-  console.log(`Members of ${name}:`)
-  do {
-    console.log(`  Level ${level++}:`)
-    for(const memberName in proto) {
-      //console.log('in for()')
-      if (proto.hasOwnProperty(memberName)) {
-        //console.log(`in if() ${memberName}`)
-        // memberName 'text' crashes sim
-        if (types)
-          try {
-            //console.log('before obj[]')
-            const member = obj[memberName]  // get member from top-level obj rather than proto, as the latter crashes if not a function
-            //console.log(`in try member=${member}`)
-            type = typeof member
-          } catch(e) {
-            //console.log('in catch')
-            type = 'INACCESSIBLE'
-          }
-        console.log(`    ${memberName} (${type})`)
-      }
-    }
-    proto = Object.getPrototypeOf(proto)
-    console.log('  ---------------')
-  } while (proto)
-}
 //dumpProperties('test', test, true)
+
+//TODO vheck why this is not working in this structure
+// as encapsulated?
+//Inspect object key:value
+inspectObject('test.light',test.light.style)
+console.log('I try to get keys' + Object.keys(test.light))  
+
+
