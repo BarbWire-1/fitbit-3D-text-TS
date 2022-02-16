fa75278de3c175e7cec47f3a1cb956409488992e
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-02-16 18:11:38 +0100

    changed fun inspect

--- app/devTools.js
+++ app/devTools.js
@@ -1,45 +1,50 @@
 //INSPECT OBJECT PROTOTYPE CHAIN ©️ Gondwana
 
-export function dumpProperties(name, obj, types) {  // This isn't needed; it's just to show how everything links together
-    // types: try to determine type of each property: can cause hard crashes with some objects.
-    let proto = obj
-    let level = 0
-    let type = '?'
-    console.log(`Members of ${name}:`)
-    do {
-      console.log(`  Level ${level++}:`)
-      for(const memberName in proto) {
-        //console.log('in for()')
-        if (proto.hasOwnProperty(memberName)) {
-          //console.log(`in if() ${memberName}`)
-          // memberName 'text' crashes sim
-          if (types)
-            try {
-              //console.log('before obj[]')
-              const member = obj[memberName]  // get member from top-level obj rather than proto, as the latter crashes if not a function
-              //console.log(`in try member=${member}`)
-              type = typeof member
-            } catch(e) {
-              //console.log('in catch')
-              type = 'INACCESSIBLE'
-            }
-          console.log(`    ${memberName} (${type})`)
-        }
+// INSPECT PROTOTYPE CHAIN ©️ Gondwana
+export function dumpProperties(name, obj, types) { 
+  // types: try to determine type of each property: can cause hard crashes with some objects.
+  let proto = obj
+  let level = 0
+  let type = '?'
+  console.log(`Members of ${name}:`)
+  do {
+    console.log(`  Level ${level++}:`)
+    for(const memberName in proto) {
+      //console.log('in for()')
+      if (proto.hasOwnProperty(memberName)) {
+        //console.log(`in if() ${memberName}`)
+        // memberName 'text' crashes sim
+        if (types)
+          try {
+            //console.log('before obj[]')
+            const member = obj[memberName]  // get member from top-level obj rather than proto, as the latter crashes if not a function
+            //console.log(`in try member=${member}`)
+            type = typeof member
+          } catch(e) {
+            //console.log('in catch')
+            type = 'INACCESSIBLE'
+          }
+        console.log(`    ${memberName} (${type})`)
       }
-      proto = Object.getPrototypeOf(proto)
-      console.log('  ---------------')
-    } while (proto)
-  }
+    }
+    proto = Object.getPrototypeOf(proto)
+    console.log('  ---------------')
+  } while (proto)
+};
+//call like: dumpProperties('obj', obj, boolean)
+
+//INSPECT key:value
+export const inspectObject = (objName,obj) => { 
   
-  //call like: dumpProperties('name', obj, boolean)
+  let keys = Object.keys(obj);//only for now
+  console.log(objName+" keys: "+ keys);////test.light keys: style,x,y 
   
-  //INSPECT key:value
-  export const inspectObject = (objName, obj) => {
-     
-    for (const prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-         
-        console.log(` ${prop}: ${JSON.stringify(obj[prop])}`)
-      };
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      
+      console.log(` ${objName} ${prop}: ${JSON.stringify(obj[prop])}`)
     };
   };
+};
+//call like: inspectObject('objName',obj)
+//TODO How to get to the properties of properties by inspecting the main object/element???
