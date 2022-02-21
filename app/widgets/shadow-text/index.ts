066014a3904bc4cfd02b2bc97fd600353ba384dc
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-02-21 14:57:19 +0100

    writes, but not readable

--- app/widgets/shadow-text/index.ts
+++ app/widgets/shadow-text/index.ts
@@ -6,19 +6,19 @@ export interface ShadowTextWidget extends TextElement {
     light: SubText;
     shadow: SubText;
     redraw();
+   
   };
   
   // DEFAULTS in widgets/shadow-text/styles.css
   // this allows them to get overwritten from main CSS if set there
   
   // mainEl.style.fill = "grey"
-  // x,y center screen
-  // textAnchor middle
+  // x,y 0
+  // textAnchor start
   // lightEl.style.fill = "white", offset -1/-1
   // shadowEl.style.fill = "red", offset 1/1
   
-  // SubText limits exposed properties
-  // as this is outside the closure, it can be modified AND logged in index.ts!!!
+    // SubText limits exposed properties
   type SubText =  {
     style: {
       fill: string,
@@ -26,110 +26,77 @@ export interface ShadowTextWidget extends TextElement {
       display: "inline" | "none" | "hidden"
     },
     x: number,
-    y: number
+    y: number,
   };
-  
-  const construct = (el: ShadowTextWidget) => {
+
+const construct = (el: ShadowTextWidget) => {
     
-    const lightEl = el.getElementById('light') as TextElement;
-    const shadowEl = el.getElementById('shadow') as TextElement;
-    const mainEl = el.getElementById('main') as TextElement;
+    const lightEl = el.getElementById('light') as unknown as TextElement;
+    const shadowEl = el.getElementById('shadow') as unknown as TextElement;
+    const mainEl = el.getElementById('main') as unknown as TextElement;
     
     
     
     // PROPERTIES
-    
-    // FIX TEXT-PROPERTIES 
-    // (same for all elements of instance)
-    // redraw on newValue
-    Object.defineProperty(el, 'text', {     
-      set(newValue) {
-        mainEl.text = newValue;
-        el.redraw();
-      }
-    });
-    Object.defineProperty(el, 'letterSpacing', {
-        set(newValue) {
-          mainEl.letterSpacing = newValue;
-          el.redraw();    
-        }
-    });
-    Object.defineProperty(el, 'textAnchor', {
-        set(newValue) {
-          mainEl.textAnchor = newValue;
-          el.redraw();    
-        }
-    });
-  //   Object.defineProperty(el, 'fontSize', {
-  //     set(newValue) {
-  //       mainEl.style.fontSize = newValue;
-  //       el.redraw();    
-  //     }
-  // });
-  
-  const defProps = (exposed, target)=> {
-   
-    Object.defineProperty(el, exposed, {
-      set(newValue) {
-        exposed = {};
-        target.exposed = newValue;
-        el.redraw();    
-      }
-    }); 
-  };
-  
-  //defProps('letterSpacing' , mainEl) 
-  //TODO this doesn't work on instance, but would on main if Textelement
-  //what I REALLY do want is to target the instance as it seems to do in the above way
-
-    // PASS PROPERTIES FROM EXPOSED TO INNER EL
-    const assignProps = (exposed, target)=> {
-      Object.defineProperty(el, exposed,{
-        get() { return target;}
+    const getProperties = (prop, obj)=> {
+      Object.defineProperty(el, prop,{
+        get() { return obj;}
       }); 
     };
-    //TODO write a .forEach using className, nameEl?
-    assignProps('main', mainEl);
-    assignProps('light', lightEl);
-    assignProps('shadow', shadowEl);
-    // PRIVATE FUNCTIONS
-    // Because the widget is a closure, functions declared here aren't accessible to code outside the widget.
+    
+    getProperties('main', mainEl);
+    getProperties('light', lightEl);
+    getProperties('shadow', shadowEl);
+  
+    
+    const setProperties = (prop,obj)=> {
+      Object.defineProperty(el, prop, {
+        set(newValue) {
+          obj[prop] = newValue;
+          el.redraw();    
+        }
+      }); 
+    };
+    
+    // PASS VALUES TO mainEl
+    // (fontSize seems to be directly on the use) //??  
+    setProperties('text', mainEl);
+    setProperties('textAnchor', mainEl);
+    setProperties('letterSpacing', mainEl);
+    
+    // EQUAL ALL TEXT (text, font) mainEL
+    const textElements = el.getElementsByClassName("myText") as TextElement[];
     el.redraw = () => { 
-      let textAnchor: string;
-        // here text-properties get assigned to all el of widget-instance
-        // although subTexts don't have EXPOSED textprops, they do have, as linked to SVG TextElement??
-        el.getElementsByClassName("myText").forEach((e: TextElement) => {
-          e.text = mainEl.text ?? "TEXT"; 
+      
+       textElements.forEach((e: TextElement) => {
+          e.text = mainEl.text ?? "fitbit"; 
           e.letterSpacing = mainEl.letterSpacing ?? 0;
           e.style.fontFamily = mainEl.style.fontFamily;
-          try {     // textEl.textAnchor throws an error if textAnchor not defined
+          try {     // mainEl.textAnchor throws an error if textAnchor not defined
             e.textAnchor = mainEl.textAnchor;
           } catch(e) {
             e.textAnchor = 'start';  // default
           };
-          // try {     // textEl.textAnchor throws an error if textAnchor not defined
-          //   e.style.fontSize = mainEl.style.fontSize;
-          // } catch(e) {
-          //   e.style.fontsize= 30;  // default
-          // }
           // unfortunately doesn't work for fontSize
-            //e.style.fontSize = mainEl.style.fontSize; 
-            //TODO check, why if set this, nothing gets displayed
+          //e.style.fontSize = mainEl.style.fontSize; 
+          //TODO check, why if set this, nothing gets displayed
         });
     
     //fix main at x,y of <use>
     mainEl.x = mainEl.y = 0;
     };
-    el.redraw();
-    
-   
-  //dumpProperties('lightEl', lightEl, false)
+  
+  
+  el.redraw();
+  // no acces to any data in this structure 🚫
+  dumpProperties('mainEl', mainEl, false)
    //crashes sim: Invalid attribute: 0x206 for SVGELEM type: 0x3706f41b
    // if tyes == true
    
-  inspectObject('light', lightEl)
+  inspectObject('main', mainEl)
+    
   
-    return el;
+  return el;
   };
   
   
