13d9ec2f048e548f7a8f4960a17c3e64f035918e
Gondwana Software <gondwanasoft@users.noreply.github.com>
2022-02-24 09:16:39 +1100

    Minor fixes and TODO updates

--- app/widgets/shadow-text/index.js
+++ app/widgets/shadow-text/index.js
@@ -12,7 +12,7 @@ const construct = (el) => {
     let lightEl = el.getElementById('light');
     let shadowEl = el.getElementById('shadow');
     let elStyle = el.style;   // keep a reference to the REAL .style because we're going to redefine .style
-    
+
     //APPLY CHANGES ON EL TO ALL
     const setNewTextAll = (obj, prop) => {
         Object.defineProperty(obj, prop, {
@@ -26,13 +26,14 @@ const construct = (el) => {
         });
 
     };
-    
+
     setNewTextAll(el, 'text');
     setNewTextAll(el, 'textAnchor');
     setNewTextAll(el, 'letterSpacing');
-    
-    
-    //APPLY TEXT-STYLE CHANGES TO ALL 
+
+
+    //APPLY TEXT-STYLE CHANGES TO ALL
+    // TODO P 3.3 study setNewStyleAll
     const setNewStyleAll = (obj, prop) => {
         Object.defineProperty(obj, prop, {
             set(newValue) {
@@ -44,7 +45,7 @@ const construct = (el) => {
             enumerable: true
         });
     };
-  
+
 
     class StyleCommon {     // style properties common to all elements
         constructor(styleBase) {
@@ -81,14 +82,14 @@ const construct = (el) => {
     };
 
     let mainAPI = Object.seal({
-        style: Object.seal(new StyleSubText(mainEl.style)),
+        style: Object.seal(new StyleSubText(mainEl.style))
     });
 
     let effectsAPI = (obj) => Object.seal({
         style: Object.seal(new StyleSubText(obj.style)),
         set x(newValue) { obj.x = newValue; },
         set y(newValue) { obj.y = newValue; },
-        enumerable: true,
+        enumerable: true
     });
 
     let widgetStyleAPI = Object.seal(new StyleWidget(elStyle));
@@ -109,20 +110,20 @@ const construct = (el) => {
     defineProps('main', mainAPI);
     defineProps('light', effectsAPI(lightEl));
     defineProps('shadow', effectsAPI(shadowEl));
-    
+
 
     // DEFINES RELATIONS BETWEEN SUBTEXTELEMENTS
     const allSubTextElements = el.getElementsByClassName('myText')
    !function assignOnLoad () {
-        
+        // TODO P 3.5 resolve letterSpacing in assignOnLoad() allSubTextElements loop; in general, lack of clarity/consistency in where SVG/CSS is to be set?
         allSubTextElements.forEach(e => {
             e.text = mainEl.text ?? "shadow-text";
             e.letterSpacing = mainEl.letterSpacing ?? 0;    // TODO should mainEl be el? Why does this work on letterSpacing, and/but only there?
-            e.style.fontFamily = mainEl.style.fontFamily;   
+            e.style.fontFamily = mainEl.style.fontFamily;
             try {     // textEl.textAnchor throws an error if textAnchor not defined
                 e.textAnchor = mainEl.textAnchor;
             } catch (e) {
-               e.Anchor = 'start';
+               e.textAnchor = 'start';
             }
             e.style.fontSize = elStyle.fontSize > 0 ? elStyle.fontSize : 30;   // if fontSize is undefined its value is -32768
         });
@@ -130,8 +131,10 @@ const construct = (el) => {
     // TODO P I checked setting to el, but it is not possible in this level (the text inheritance, I assume)
     // TODO B ^ You're right about letterSpacing, which can't be set on use in SVG/CSS. fontFamily could perhaps be set on use or main in SVG/CSS, so may need a conscious decision about which to copy above.
     // TODO P ^I'm not sure whether it makes sense to make it an IIFE, just seemed logical, but requires an outer var
-    
-    
+    // TODO P 3.0 make IIFE anonymous (no 'outer val'): https://stackoverflow.com/questions/3755606/what-does-the-exclamation-mark-do-before-the-function
+    // TODO P 3.1 put config into IIFE?
+
+
     // INITIALISATION:
     // Parse and process SVG config attributes:
     const attributes = el.getElementById('config').text.split(';')
@@ -184,3 +187,6 @@ you now can see: symbol defaults => js => css applied
 
 I guess one of my recent changes might have caused that, but can't check now as out... sorry
 */
+// TODO B ^ How are you measuring the speed? ...
+// TODO B ...Are you sure this isn't an issue arising from lack of clarity about which props are set against use and which against main?...
+// TODO B ...You could console.log in various places to see the sequence in which things are getting applied; possibly the IIFE is getting processed at a different time than previously.
