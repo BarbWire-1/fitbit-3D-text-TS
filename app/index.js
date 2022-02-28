1954fd417e767585bf20043854bcae176470e748
Gondwana Software <gondwanasoft@users.noreply.github.com>
2022-02-28 14:47:25 +1100

    My other homework TODOs

--- app/index.js
+++ app/index.js
@@ -31,6 +31,8 @@ calsLabel.textAnchor = "middle"
 //* TESTING *******************************************************************************************
 // GETTERS
 //TODO P^ widget defaults don't get logged here
+//TODO B ^ I'm not sure what you mean. Props with uninitialised values don't usually report default or inherited values; see:
+console.log(`normalText.x = ${document.getElementById('normalText').x}`)
 // integrate defaults in getters?
 // I had tried to change sequence, ie setting initialisation on top of the construct(), but then there are some elements undefined
 test.style.fontSize = 50;
@@ -49,6 +51,9 @@ test.shadow.y = 5
 //test.main.style.display = 'none'
 //TODO P ^ this gets also processed if display = "none"???
 //TODO P ^ practcal, but confusing
+//TODO B ^ I think I see your point, but I feel that it has to be this way. widget.display and each subText.display needs to be independently controllable...
+//TODO B ^ ...and in any order. Big-time uses both properties independently and successfully.
+//TODO B ^ ...I suspect if you put a display='inline' element inside a display='none' <svg>, you won't see your element.
 
 
 //test.style.fontFamily = "Tungsten-Medium"
@@ -83,7 +88,7 @@ inspectObject('test.style', test.style)// test.main.style.fill: undefined
 
 //TODO P^I wonder about the best place to apply Object.seal(). Had you tested that?
 // as there are some set on outer (API) and inner like 'style'
-
+// TODO B ^ Yes, I tested (briefly). Object.seal only seals the object to which it is applied, and not any objects contained within it. I think; feel free to research and test!
 
 
 
